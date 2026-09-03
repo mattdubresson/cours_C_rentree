4173baa86a04ac91396458e970360ab3028f161f
Matteo Dubresson <mattdubresson@gmail.com>
2026-09-03 17:37:59 +0200

    appel à un ami

--- main.c
+++ main.c
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 
 // SEANCE 2
 
-
+/*
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -260,7 +260,161 @@ void affichage(uint32_t etat_grille){
 }
 
 
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+int extraction_bits(int num_case, uint32_t etat_grille);
+uint32_t poser_jeton(uint32_t etat_grille, int num_case, int joueur);
+int victoire(uint32_t etat_grille);
+char attribution_case(int n, uint32_t etat_grille);
+void affichage(uint32_t etat_grille);
 
+int main(void){
+
+int num_case = 0;
+int num_joueur = 1;
+int val_joueur;
+uint32_t etat_grille = 0;
+int nb_coups = 0;
+
+while (victoire(etat_grille) == -1 && nb_coups < 9) {
+    affichage(etat_grille);
+    printf("Joueur %d ? ", num_joueur);
+    val_joueur = scanf("%d", &num_case);
+
+    if (val_joueur == 1 && num_case >= 1 && num_case <= 9) {
+        int pos = num_case - 1;
+        if (extraction_bits(pos, etat_grille) == 0) {
+            etat_grille = poser_jeton(etat_grille, pos, num_joueur);
+            nb_coups++;
+            if (num_joueur == 1) {
+                num_joueur = 2;
+            } else {
+                num_joueur = 1;
+            }
+        } else {
+            printf("Case deja occupee !\n");
+        }
+    } else {
+        while (getchar() != '\n');
+        printf("Saisie invalide !\n");
+    }
+}
+
+affichage(etat_grille);
+int res = victoire(etat_grille);
+if (res != -1) {
+    printf("le gagnant est le joueur %d\n", res);
+} else {
+    printf("Match nul !\n");
+}
+
+return 0;
+
+}
+
+// fonction pour récupérer la valeur d'une case enregistrée dans le nombre etat_grille
+
+int extraction_bits(int num_case, uint32_t etat_grille){
+uint8_t valeur_recuperee = (etat_grille >> (2*num_case)) & 3 ;
+return valeur_recuperee;
+}
+
+// fonction pour modifier la valeur d'une case dans etat_grille 
+
+uint32_t poser_jeton(uint32_t etat_grille, int num_case, int joueur){
+return etat_grille | (joueur << (2 * num_case));
+}
+
+// fonction pour calculer à chaque fois si on est dans une victoire
+
+int victoire(uint32_t etat_grille){
+
+if (extraction_bits(0, etat_grille) != 0){
+    if(extraction_bits(0, etat_grille) == extraction_bits(1, etat_grille) && extraction_bits(1, etat_grille) == extraction_bits(2, etat_grille)){
+        return extraction_bits(0, etat_grille);
+    }
+}
+
+if (extraction_bits(3, etat_grille) != 0){
+    if(extraction_bits(3, etat_grille) == extraction_bits(4, etat_grille) && extraction_bits(4, etat_grille) == extraction_bits(5, etat_grille)){
+        return extraction_bits(3, etat_grille);
+    }
+}
+
+if (extraction_bits(6, etat_grille) != 0){
+    if(extraction_bits(6, etat_grille) == extraction_bits(7, etat_grille) && extraction_bits(7, etat_grille) == extraction_bits(8, etat_grille)){
+        return extraction_bits(6, etat_grille);
+    }
+}
+
+if (extraction_bits(0, etat_grille) != 0){
+    if(extraction_bits(0, etat_grille) == extraction_bits(3, etat_grille) && extraction_bits(3, etat_grille) == extraction_bits(6, etat_grille)){
+        return extraction_bits(0, etat_grille);
+    }
+}
+
+if (extraction_bits(1, etat_grille) != 0){
+    if(extraction_bits(1, etat_grille) == extraction_bits(4, etat_grille) && extraction_bits(4, etat_grille) == extraction_bits(7, etat_grille)){
+        return extraction_bits(1, etat_grille);
+    }
+}
+
+if (extraction_bits(2, etat_grille) != 0){
+    if(extraction_bits(2, etat_grille) == extraction_bits(5, etat_grille) && extraction_bits(5, etat_grille) == extraction_bits(8, etat_grille)){
+        return extraction_bits(2, etat_grille);
+    }
+}
+
+if (extraction_bits(0, etat_grille) != 0){
+    if(extraction_bits(0, etat_grille) == extraction_bits(4, etat_grille) && extraction_bits(4, etat_grille) == extraction_bits(8, etat_grille)){
+        return extraction_bits(0, etat_grille);
+    }
+}
+
+if (extraction_bits(2, etat_grille) != 0){
+    if(extraction_bits(2, etat_grille) == extraction_bits(4, etat_grille) && extraction_bits(4, etat_grille) == extraction_bits(6, etat_grille)){
+        return extraction_bits(2, etat_grille);
+    }
+}
+
+return -1;
+
+}
+
+// fonction pour donner directement quoi afficher dans chaque case en conftion de etat_grille
+
+char attribution_case(int n, uint32_t etat_grille){
+if (extraction_bits(n, etat_grille) == 0){
+return ' ';
+}
+else if (extraction_bits(n, etat_grille) == 1){
+return 'O';
+}
+else {
+return 'X';
+}
+}
+
+// fonction pour print a chaque coup l'état du morpion
+
+void affichage(uint32_t etat_grille){
+char case_0 = attribution_case(0, etat_grille);
+char case_1 = attribution_case(1, etat_grille);
+char case_2 = attribution_case(2, etat_grille);
+char case_3 = attribution_case(3, etat_grille);
+char case_4 = attribution_case(4, etat_grille);
+char case_5 = attribution_case(5, etat_grille);
+char case_6 = attribution_case(6, etat_grille);
+char case_7 = attribution_case(7, etat_grille);
+char case_8 = attribution_case(8, etat_grille);
+
+printf("%c | %c | %c\n---------\n"
+       "%c | %c | %c\n---------\n"
+       "%c | %c | %c\n---------\n", case_6, case_7, case_8, case_3, case_4, case_5, case_0, case_1, case_2);
+
+}
 
